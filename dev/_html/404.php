3e9aded1a5f6bccc0fc7998c9bb0535793bd8ac5
loki <ideinost@gmail.com>
2017-12-10 01:58:25 +0300

    page 404

---
+++ dev/_html/404.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ru">
+<?php include 'base/head.php';?>
+<body>
+    <div class="page page-index">
+        <div class="page-inner">
+            <?php include 'base/header.php';?>
+
+            <div class="wrapper">
+                <div class="page-body page-body--white">
+                    <div class="contain">
+                       <div class="notfound">
+                           <h1>Ощибка 404.<br>Страница не существует</h1>
+                           <p>Похоже такой страницы не существует. <br>Начните с <a href="/" class="link link--bottom-line">главной страницы</a></p>
+                       </div>
+                    </div>
+                </div><!-- /page-body -->
+
+            </div>
+
+            <?php include 'base/footer.php';?>
+        </div><!-- /page-inner -->
+
+        <!-- popups -->
+
+    </div><!-- /page -->
+
+    <?php include 'base/scripts.php'; ?>
+</body>
+</html>
