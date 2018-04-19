a3b3c7d089c60242d7179fbc6d73737a3715b117
loki <ideinost@gmail.com>
2018-04-19 13:36:03 +0300

    page feedback done

--- dev/_html/feedback.php
+++ dev/_html/feedback.php
@@ -11,47 +11,53 @@
 
             <div class="wrapper">
                 <div class="page-body page-body--white">
-                        <div class="title--fill title--fill--min h-mrg-t-62">
-                            ОБРАТНАЯ СВЯЗЬ
+                        <div class="feedback-contain1">
+                            <div class="title--fill title--fill--min">
+                                ОБРАТНАЯ СВЯЗЬ
+                            </div>
+
+                            <form action="/" class="form-feedback h-mrg-t-35 js-form">
+                                <div class="form-feedback__row">
+                                    <input type="text" name='feedback[name]' class="input" placeholder="Ваша фамилия и имя" >
+                                </div>
+                                <div class="form-feedback__row">
+                                    <input type="email" name='feedback[email]' class="input" placeholder="Эл. почта" >
+                                </div>
+                                <div class="form-feedback__row">
+                                    <input type="tel" name='feedback[phone]' class="input" placeholder="Контактный телефон*" required>
+                                </div>
+                                <div class="form-feedback__row">
+                                    <input type="text" name='feedback[location]' class="input" placeholder="Местонахождение*" required>
+                                </div>
+                                <div class="form-feedback__row">
+                                    <input type="text" name='feedback[mail_address]' class="input" placeholder="Почтовый адрес" >
+                                </div>
+                                <div class="form-feedback__row">
+                                    <input type="text" name='feedback[subject]' class="input" placeholder="Тема письма" >
+                                </div>
+                                <div class="form-feedback__row">
+                                    <textarea name="feedback[message]" id="" cols="30" rows="4" class="textarea" placeholder="Соообщение"></textarea>
+                                </div>
+                                <div class="form-feedback__footer">
+                                    <div class="term-agree-checkbox">
+                                        <div class="checkbox">
+                                            <input id="agree" type="checkbox">
+                                            <label for="agree"></label>
+                                        </div>
+                                        <p>Отправляя заявку, я принимаю <a href="#!" class="link link--blue">Пользовательское соглашение</a></p>
+                                    </div>
+                                    <button type='submit' class='btn btn--feedback-submit'>Отправить</button>
+                                </div>
+                                <div class="js-form__all-msg"></div>
+                            </form>
                         </div>
 
-                        <form action="/" class="form-feedback h-mrg-t-35 js-form">
-                            <div class="form-feedback__row">
-                                <input type="text" name='feedback[name]' class="input" placeholder="Ваша фамилия и имя" >
-                            </div>
-                            <div class="form-feedback__row">
-                                <input type="email" name='feedback[email]' class="input" placeholder="Эл. почта" >
-                            </div>
-                            <div class="form-feedback__row">
-                                <input type="tel" name='feedback[phone]' class="input" placeholder="Контактный телефон*" required>
-                            </div>
-                            <div class="form-feedback__row">
-                                <input type="text" name='feedback[location]' class="input" placeholder="Местонахождение*" required>
-                            </div>
-                            <div class="form-feedback__row">
-                                <input type="text" name='feedback[mail_address]' class="input" placeholder="Почтовый адрес" >
-                            </div>
-                            <div class="form-feedback__row">
-                                <input type="text" name='feedback[subject]' class="input" placeholder="Тема письма" >
-                            </div>
-                            <div class="form-feedback__row">
-                                <textarea name="feedback[message]" id="" cols="30" rows="4" class="textarea" placeholder="Соообщение"></textarea>
-                            </div>
-                            <div class="form-feedback__row h-alg-r">
-                                <button type='submit' class='btn btn--feedback-submit'>Отправить</button>
-                            </div>
-                            <div class="h-mrg-t-10">
-                              <div class="js-form__all-msg"></div>
-                            </div>
-                        </form>
-
-                        <div class="box-shadow box-shadow--pad3 h-mrg-t-15 h-mrg-b-48">
+                        <div class="feedback-contain2 box-shadow box-shadow--pad3">
                             <div class="form-feedback__row form-feedback__row--no-m">
-                                <div class="box-shadow__line h-mrg-b-5"></div>
                                 <div class="cols">
                                     <div class="col--70">
-                                        <p class='box-shadow__p h-mrg-t-5'>КОНТАКТЫ   ОБРАТНАЯ СВЯЗЬ</p>
-                                        <p class='box-shadow__p h-mrg-t-5'>ГОРЯЧАЯ ЛИНИЯ 8-800-200-70-80 (бесплатный звонок по России)</p>
+                                        <p class='box-shadow__p'>КОНТАКТЫ   ОБРАТНАЯ СВЯЗЬ</p>
+                                        <p class='box-shadow__p'>ГОРЯЧАЯ ЛИНИЯ 8-800-200-70-80 (бесплатный звонок по России)</p>
                                     </div>
                                     <div class="col--30 h-alg-r">
                                         <button data-mfp-src='#popup-mailing' type='submit' class='btn btn--feedback-submit btn--feedback-submit--min js-link-popup'>
